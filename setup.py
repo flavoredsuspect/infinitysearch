b7b79f5f4421b82c70da967adc7b5f616cbb1d90
flavoredsuspect <flavoredsuspect@gmail.com>
2025-05-27 13:18:31 -0400

    Setup nice build

--- setup.py
+++ setup.py
@@ -1,39 +1,43 @@
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
 import pybind11
 
 ext_modules = [
     Extension(
-        'vp_tree',  # C++ extension name
-        ['cpp_extension/vp_tree_bind.cpp'],
+        'vp_tree',
+        sources=['cpp_extension/vp_tree_bind.cpp'],
         include_dirs=[pybind11.get_include()],
         language='c++',
-        extra_compile_args=['-std=c++17']
-    ),
+        extra_compile_args=[
+            '-std=c++17',
+            '-mavx',
+            '-mfma'
+        ]
+    )
 ]
 
 setup(
-    name='infinitysearch',                 # Library name
-    version='0.1.0',                       # Version number
-    author='Your Name',                    # Author details
-    author_email='youremail@example.com',
-    description='Fermat-based nearest neighbor search with VP-tree backend',
+    name='infinitysearch',
+    version='0.1.0',
+    description='Fermat-based approximate nearest neighbor search with VP-tree backend',
     long_description=open("README.md").read(),
-    long_description_content_type='text/markdown',  # README format
-    packages=['infinitysearch'],           # The folder containing your code
-    ext_modules=ext_modules,               # Include C++ extensions
-    install_requires=[                     # Dependencies
+    long_description_content_type='text/markdown',
+    author='Antonio Pariente',
+    license='Custom Non-Commercial Patent License',
+    packages=['infinitysearch'],
+    ext_modules=ext_modules,
+    install_requires=[
         'numpy',
         'scipy',
         'torch',
-        'scikit-learn',
         'tensorflow',
-        'pybind11',
+        'scikit-learn',
+        'pybind11'
     ],
-    python_requires='>=3.7',               # Ensure compatibility with Python 3.7+
+    python_requires='>=3.9',
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent"
     ],
 )
 
